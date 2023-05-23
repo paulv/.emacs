fb873f853c8d97e44a8e3ccb9a557bed5986dd13
Paul Visscher <paulv@canonical.org>
2023-05-23 17:58:25 -0400

    no idea why this changed

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (magit web-mode groovy-mode which-key smex use-package typescript-mode paradox lsp-ui exec-path-from-shell dracula-theme company-lsp flycheck transient vterm vterm-toggle ido-completing-read+ inf-ruby lsp-mode lua-mode markdown-mode multiple-cursors pkg-info popup puppet-mode ruby-electric ruby-hash-syntax ruby-tools smex solarized-theme terraform-mode typescript-mode yaml-mode yasnippet zenburn-theme dracula-theme async cl-lib company dockerfile-mode go-mode js2-mode js2-refactor json-mode json-reformat tern ## tide browse-kill-ring company-tern ruby-additional zenburn tempo-snippets ruby-mode jira hideshowvis css-mode smex use-package typescript-mode paradox lsp-ui exec-path-from-shell dracula-theme)))
+   '(magit web-mode groovy-mode which-key smex use-package typescript-mode paradox lsp-ui exec-path-from-shell dracula-theme company-lsp flycheck transient vterm vterm-toggle ido-completing-read+ inf-ruby lsp-mode lua-mode markdown-mode multiple-cursors pkg-info popup puppet-mode ruby-electric ruby-hash-syntax ruby-tools smex solarized-theme terraform-mode typescript-mode yaml-mode yasnippet zenburn-theme dracula-theme async cl-lib company dockerfile-mode go-mode js2-mode js2-refactor json-mode json-reformat tern ## tide browse-kill-ring company-tern ruby-additional zenburn tempo-snippets ruby-mode jira hideshowvis css-mode smex use-package typescript-mode paradox lsp-ui exec-path-from-shell dracula-theme))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
